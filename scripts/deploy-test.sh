e766cfebe117680614b38affa060f2d0f9f80c03
peterajessup <peterajessup@users.noreply.github.com>
2023-05-11 17:24:26 +1000

    Update deploy-test.sh

--- scripts/deploy-test.sh
+++ scripts/deploy-test.sh
@@ -1,4 +1,4 @@
 oc project mq
-oc  wait -n mq pod --for=condition=Ready -l testTarget=mq2
+oc  wait -n mq pod --for=condition=Ready -l testTarget=mq2 --timeout=100s
 oc apply -f test/app-subscriber-service.yaml
 oc apply -f test/mq-tester.yaml --wait
