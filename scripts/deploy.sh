1af2b114443645a30fa9be211ac4cbe4b082bc29
peterajessup <peterajessup@users.noreply.github.com>
2024-01-18 13:48:45 +1100

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -13,7 +13,7 @@ oc delete configMap mq2-mqsc
 set -e
 # Create the route and the keystore secret and mqsc configMap
 oc apply -f mq2Route.yaml
-# oc create secret tls mq2key --cert=./tls/tls.crt --key=./tls/tls.key
+ oc create secret tls mq2key --cert=./tls/qm.crt --key=./tls/qm.key
 oc create secret generic tls-trust --from-file ./tls/tls.crt
 oc create -f mqsc/mqsc.yaml
 
